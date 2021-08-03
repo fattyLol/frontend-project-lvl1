506dc45163b1c80043f927c294c95c9d52a53d19
Petr <Petrtelepaev@yandex.ru>
2021-08-03 12:20:31 +0300

    lvl 4, add linter, CI

--- src/cli/greeter.js
+++ src/cli/greeter.js
@@ -1,13 +1,14 @@
 import readline from 'readline';
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
-export const greet = () => {
-    rl.question('May I ask your name? ', (name) => {
-        console.log(`Hello, dear ${name}`);
-    });
-}
+const greet = () => {
+  rl.question('May I ask your name? ', (name) => {
+    console.log(`Hello, dear ${name}`);
+  });
+};
 
+export default greet;
